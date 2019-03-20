51960c8242faadf9809b6de1052eb3d87ff4f1af
Dan Eden <dje2402@rit.edu>
2019-03-20 16:09:34 -0400

    created error logging in controller for card search and card details

--- mtg.py
+++ mtg.py
@@ -20,6 +20,9 @@ import card
 # Import SQLite3
 import sqlite3
 
+#import error logging model
+from errorLog import logError
+
 conn = sqlite3.connect('DB Scripts/mtg.sqlite3')
 
 ##################################################################################
@@ -48,21 +51,30 @@ def search():
 	search_name = request.form.get('search')
 
 	# Get some cards
-	cards = Card.where(page=1).where(pageSize=40).where(name=search_name).all()
+	try:
+		cards = Card.where(page=1).where(pageSize=40).where(name=search_name).all()
 
-	# Set card name list to sent to template
-	c_names = []
+		# Set card name list to sent to template
+		c_names = []
 
-	# # Put card names in list
-	for c in cards:
-		if str(c.image_url) != "None":
-			c_names.append([c.name, c.image_url, c.multiverse_id])
+		# # Put card names in list
+		for c in cards:
+			if str(c.image_url) != "None":
+				c_names.append([c.name, c.image_url, c.multiverse_id])
 
-	return render_template('search.html',
-		title=title,
-		cards=c_names
-	)
+		return render_template('search.html',
+			title=title,
+			cards=c_names
+		)
 
+	# catch and log error
+	except Exception as ex:
+		logError("API Get Card Search",ex)
+
+		return render_template('search.html',
+			title=title,
+			cards="error"
+		)
 ##################################################################################
 # Card Details Page
 @app.route("/card/<string:card_id>", methods=['GET','POST'])
@@ -71,32 +83,43 @@ def card(card_id):
 	title = "MTG Deck Planner | Card Details"
 
 	# card card details from API based on multiverse ID
-	api_card = Card.find(card_id)
+	try:
+		api_card = Card.find(card_id)
 
-	return render_template('card.html',
-		title=title,
+		return render_template('card.html',
+			title=title,
 
-		name = api_card.name,
-		multiverse_id = api_card.multiverse_id,
-		layout = api_card.layout,
-		mana_cost = api_card.mana_cost,
-		cmc = api_card.cmc,
-		colors = api_card.colors,
-		color_identity = api_card.color_identity,
-		type = api_card.type,
-		supertypes = api_card.supertypes,
-		subtypes = api_card.subtypes,
-		rarity = api_card.rarity,
-		text = api_card.text,
-		flavor = api_card.flavor,
-		power = api_card.power,
-		toughness = api_card.toughness,
-		life = api_card.life,
-		legalities = api_card.legalities,
-		image_url = api_card.image_url,
-		set = api_card.set,
-		set_name = api_card.set_name
-	)
+			name = api_card.name,
+			multiverse_id = api_card.multiverse_id,
+			layout = api_card.layout,
+			mana_cost = api_card.mana_cost,
+			cmc = api_card.cmc,
+			colors = api_card.colors,
+			color_identity = api_card.color_identity,
+			type = api_card.type,
+			supertypes = api_card.supertypes,
+			subtypes = api_card.subtypes,
+			rarity = api_card.rarity,
+			text = api_card.text,
+			flavor = api_card.flavor,
+			power = api_card.power,
+			toughness = api_card.toughness,
+			life = api_card.life,
+			legalities = api_card.legalities,
+			image_url = api_card.image_url,
+			set = api_card.set,
+			set_name = api_card.set_name
+		)
+
+	# catch and log error
+	except Exception as ex:
+		logError("API Get Card Details",ex)
+
+		return render_template('card.html',
+			title=title,
+
+			name = "error",
+		)
 
 ##################################################################################
 # SIgnUP
