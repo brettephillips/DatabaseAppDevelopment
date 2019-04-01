3c5e0098c9231d33868d71a59ea8c547895dcc3d
Brett Phillips <bep4144@rit.edu>
2019-04-01 14:33:43 -0400

    Migrating From SQLite DB
    Changing the DB script from SQLite to PostgreSQL

--- DB Scripts/createTables.sql
+++ DB Scripts/createTables.sql
@@ -1,25 +1,35 @@
+DROP DATABASE IF EXISTS MTG;
+CREATE DATABASE MTG;
+
+DROP TABLE IF EXISTS USER CASCADE;
 CREATE TABLE USER(
-  user_id INTEGER primary key AUTOINCREMENT,
-  username TEXT,
-  password TEXT
+  user_id INT SERIAL NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  PRIMARY KEY(user_id)
 );
 
+DROP TABLE IF EXISTS DECK CASCADE;
 CREATE TABLE DECK(
-  deck_id INTEGER primary key AUTOINCREMENT,
-  name TEXT,
-  rating INTEGER,
-  user_id INTEGER,
-  FOREIGN KEY(user_id) REFERENCES USER(user_id)
+  deck_id INT SERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  rating INT,
+  user_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES USER(user_id),
+  PRIMARy KEY(deck_id)
 );
 
+DROP TABLE IF EXISTS CARD CASCADE;
 CREATE TABLE CARD(
-  card_id integer primary key AUTOINCREMENT,
-  api_id integer
+  card_id INT SERIAL NOT NULL,
+  api_id INT ONT NULL,
+  PRIMARY KEY(card_id)
 );
 
+DROP TABLE IF EXISTS DECK_CARD CASCADE;
 CREATE TABLE DECK_CARD(
-  card_id INTEGER,
-  deck_id INTEGER,
+  card_id INT NOT NULL,
+  deck_id INT NOT NULL,
   FOREIGN KEY(card_id) REFERENCES CARD(card_id),
   FOREIGN KEY(deck_id) REFERENCES DECK(deck_id),
   PRIMARY KEY(card_id, deck_id)
